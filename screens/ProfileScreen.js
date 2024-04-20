a5a53e6677b7b7386adef35324e40da147d22b63
sharonc05 <sharonxc@g.ucla.edu>
2024-04-20 11:48:14 -0700

    continued with fixes

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -19,8 +19,6 @@ const Profile = ({ navigation }) => {
    // Let the user pick an image from their library
    const result = await ImagePicker.launchImageLibraryAsync({
      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-     allowsEditing: true,
-     aspect: [1, 1],
      quality: 1,
    });
 
