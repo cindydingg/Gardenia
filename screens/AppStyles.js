5772f0ddd364783352a5a8cb1b9a1935edadb153
sharonc05 <sharonxc@g.ucla.edu>
2024-04-20 11:22:32 -0700

    added Login, AppStyles, profile styling to fiona changes

---
+++ screens/AppStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: '#FFFFFF', // Full background color
+   alignItems: 'center',
+   justifyContent: 'center',
+   padding: 20,
+   marginBottom: 0,
+ },
+ headline: {
+   fontSize: 28,
+   color: '#219653',
+   marginBottom: 50,
+   fontFamily: 'Poppins',
+   fontWeight: '500',
+ },
+ input: {
+   height: 50,
+   width: '90%',
+   marginVertical: 10,
+   borderWidth: 1,
+   padding: 10,
+   borderColor: '#C4C4C4',
+   borderRadius: 40,
+   backgroundColor: '#FFFFFF',
+   color: '#000000',
+   fontFamily: 'Poppins',
+   fontSize: 14,
+ },
+ image: {
+   width: 300,
+   height: 300,
+   marginTop: 20,
+ },
+
+
+});
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
+import styles from './AppStyles'; // Make sure the path is correct
+
 
 const SignUp = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>Sign up Page!</Text>
-      <Button title="sign up" onPress={() => navigation.navigate('Profile')} />
-    </View>
-  );
+ return (
+   <View style={styles.container}>
+     <Text style={styles.headline}>Sign up Page</Text>
+     <TextInput
+       style={styles.input}
+       placeholder="Username"
+       placeholderTextColor="#219653"
+     />
+     <TextInput
+       style={styles.input}
+       placeholder="Password"
+       placeholderTextColor="#219653"
+       secureTextEntry={true}
+     />
+     <Button
+       title="sign up"
+       onPress={() => navigation.navigate('Profile')}
+       color="#6FCF97"
+     />
+   </View>
+ );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default SignUp;
