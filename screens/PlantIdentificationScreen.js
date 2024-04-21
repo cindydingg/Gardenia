1ece918500b508330eaab9d2f83cb7aed1b310df
Selina Song <selinasong6@gmail.com>
2024-04-20 19:31:31 -0700

    Improved Plantidentification UI
    SELINA ATTTEEEEEE

--- screens/PlantIdentificationScreen.js
+++ screens/PlantIdentificationScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions, TouchableOpacity } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
@@ -7,13 +7,31 @@ const PlantIdentificationScreen = ({ route, navigation }) => {
   //retrieve classification result passed as a param
   const { result } = route.params;
 
+  // Function to parse the percentage and calculate points
+  const parsePoints = (resultString) => {
+    const regex = /(\d+)%/; // Regular expression to find a number followed by '%'
+    const match = resultString.match(regex);
+    if (match && match[1]) {
+      const percentage = parseInt(match[1], 10); // Convert the percentage to an integer
+      return percentage * 10; // Return the points calculated by multiplying by 10
+    }
+    return 0; // Return 0 if no percentage is found
+  };
+
+  // Calculate points from the result
+  const points = parsePoints(result);
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Your plant has been identified:</Text>
       <Text style={styles.result}>{result || 'No result available'}</Text>
-      <View style={styles.buttonContainer} > 
-      <Button title="Go Back to Upload" onPress={() => navigation.navigate('Upload')} />
-      </View>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('Upload')} 
+      > 
+
+      <Text style={styles.backButtonText}>Go Back to Upload</Text>
+      </TouchableOpacity>
+      <Text style={styles.header}>Points Gained: </Text>
+      <Text style={styles.result}>{points || 'No points available'}</Text>
     </View>
   );
 };
@@ -24,6 +42,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backButton: {
+    backgroundColor: '#6FCF97', // Feel free to change the background color
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  backButtonText: {
+    color: '#FFFFFF', // Making text color white
+    fontSize: 16,
+    fontFamily: 'Poppins', // Ensure you have this font loaded if you use it
   },
   header: {
     fontSize: 18,
@@ -41,7 +71,7 @@ const styles = StyleSheet.create({
     height: 50, // Height as per your specification
     left: (width - 204.93) / 2, // Center horizontally based on container width
     top: 214, // Position from the top as specified
-    backgroundColor: 'lightgray', // Background color for visibility
+    backgroundColor: 'gray', // Background color for visibility
     justifyContent: 'center',
     borderRadius: 25, // Optional: if you want rounded buttons
   }
